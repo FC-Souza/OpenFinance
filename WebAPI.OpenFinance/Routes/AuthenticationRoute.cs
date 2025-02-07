f09ce6cda531f52d4acac5a94acafbeb360caab7
FC-Souza <fc-souza@outlook.com>
2025-02-07 15:58:52 -0500

    Add POST /signup endpoint for user registration
    Introduce a new POST /signup endpoint in AuthenticationRoute.cs to handle user signups. The endpoint processes the signup by extracting user details from the JSON payload, adding a new client entry to the Clients table, saving changes to generate a client ID, and adding an entry to the ClientCredentials table. A new Signup class models the JSON payload for the signup request.

--- WebAPI.OpenFinance/Routes/AuthenticationRoute.cs
+++ WebAPI.OpenFinance/Routes/AuthenticationRoute.cs
@@ -43,7 +43,57 @@ namespace WebAPI.OpenFinance.Routes
 
             });
 
-        
+            //POST /Signup
+            //Will receive a JSON with the email, password, full name and address
+            //Will return a JSON with the clientID and Full Name
+            //Password will be stored at client_credential
+            route.MapPost("/signup", async (OpenFinanceContext context, Signup signup)
+                =>
+            {
+                //Get the email, password, full name and address from the JSON
+                var email = signup.Email;
+                var password = signup.Password;
+                var name = signup.Name;
+                var address = signup.Address;
+
+                //Add the client to the clients table
+                var client = new ClientsModel
+                {
+                    clientName = name,
+                    clientEmail = email,
+                    clientAddress = address
+                };
+                context.Clients.Add(client);
+                await context.SaveChangesAsync();
+
+                //Get the client_id with the email
+                var clientID = await context.Clients
+                    .Where(c => c.clientEmail == email)
+                    .Select(c => c.clientID)
+                    .FirstOrDefaultAsync();
+
+                //Add the client to the client_credential table
+                var clientCredential = new ClientCredentialModel
+                {
+                    clientID = clientID,
+                    clientPassword = password
+                };
+                context.ClientCredentials.Add(clientCredential);
+                await context.SaveChangesAsync();
+
+                //Return the client_id and client_name
+                var signupResponse = new
+                {
+                    clientID = clientID,
+                    clientName = name
+                };
+
+                return Results.Ok(signupResponse);
+            });
+
+
+
+
         }
     }
 
@@ -52,5 +102,14 @@ namespace WebAPI.OpenFinance.Routes
     {
         public string Email { get; set; }
         public string Password { get; set; }
+    }
+
+    //Class to receive the JSON from Signup
+    public class Signup
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
     }
 }
