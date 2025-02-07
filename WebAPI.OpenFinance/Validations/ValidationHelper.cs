c9fa196633786433bbf95891547573fc9082bd5c
FC-Souza <fc-souza@outlook.com>
2025-02-07 18:03:16 -0500

    Add input validation and email uniqueness check
    Updated AuthenticationRoute.cs to include input validation for email, password, name, and address using the new ValidationHelper class. Added ValidationHelper.cs with static methods for validation using regex and length checks. Implemented checks for email uniqueness before adding a new client. Simplified clientID retrieval logic. ValidationHelper class is marked as internal.

---
+++ WebAPI.OpenFinance/Validations/ValidationHelper.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.RegularExpressions;
+
+namespace WebAPI.OpenFinance.Validations
+{
+    //Internal, because the class will be used only in this project
+    internal static class ValidationHelper
+    {
+        //Check if the email is valid using REGEX
+        internal static bool IsValidEmail(string email)
+        {
+            //Regex regexPattern for email
+            string regexPattern = @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$";
+
+            //Check the REGEX and return the result
+            return Regex.IsMatch(email, regexPattern);
+        }
+
+        //Check if the password is valid using REGEX
+        //8 characters minimum, 1 uppercase, 1 lowercase, 1 number and 1 special character
+        internal static bool IsValidPassword(string password)
+        {
+            //Regex for password
+            string regexPattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$";
+
+            //Check the REGEX and return the result
+            return Regex.IsMatch(password, regexPattern);
+        }
+
+        //Check if the name is valid using REGEX
+        //Only letters and spaces, 3 characters minimum
+        internal static bool IsValidName(string name)
+        {
+            //Regex for name
+            string regexPattern = @"^[a-zA-Z\s]{3,}$";
+
+            //Check the REGEX and return the result
+            return Regex.IsMatch(name, regexPattern);
+        }
+
+        //Check if the address is valid
+        //5 characters minimum
+        internal static bool IsValidAddress(string address)
+        {
+            //Check the length
+            return address.Length >= 5;
+        }
+
+    }
+}
