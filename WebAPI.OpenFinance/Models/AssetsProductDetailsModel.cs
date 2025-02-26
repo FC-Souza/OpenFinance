433e55bf51d030ddf6876e5546a46728d6a5167a
FC-Souza <fc-souza@outlook.com>
2025-02-25 19:38:21 -0500

    Add new properties to AssetsProduct models
    Updated AssetsProductDetailsModel to include ProductTotalAmountInvested, ProductTotalProfitLoss, and ProductTotalProfitLossPercentage properties. Updated AssetsProductItemModel to include ItemID, ItemAmountInvested, and ItemProfitLossPercentage properties. These changes enhance the tracking of investment details and performance metrics for products and individual items.

--- WebAPI.OpenFinance/Models/AssetsProductDetailsModel.cs
+++ WebAPI.OpenFinance/Models/AssetsProductDetailsModel.cs
@@ -6,6 +6,9 @@
         public int NumItems { get; set; }
         public decimal ProductTotalAmount { get; set; }
         public decimal PortfolioPercentage { get; set; }
+        public decimal ProductTotalAmountInvested { get; set; }
+        public decimal ProductTotalProfitLoss { get; set; }
+        public decimal ProductTotalProfitLossPercentage { get; set; }
         public List<AssetsProductItemModel> Items { get; set; }
     }
 }
--- WebAPI.OpenFinance/Models/AssetsProductItemModel.cs
+++ WebAPI.OpenFinance/Models/AssetsProductItemModel.cs
@@ -3,8 +3,11 @@
     public class AssetsProductItemModel
     {
         public string ItemName { get; set; }
+        public int ItemID { get; set; }
         public decimal ItemAmount { get; set; }
+        public decimal ItemAmountInvested { get; set; }
         public decimal ItemProfitLoss { get; set; }
+        public decimal ItemProfitLossPercentage { get; set; }
         public decimal PortfolioPercentage { get; set; }
     }
 }
